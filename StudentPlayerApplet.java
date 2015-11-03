03bfc5ad7a90254ee525ca13ac8092c3ccd14826
Michael Wall <thewalla07@gmail.com>
2015-11-03 00:08:05 +0000

    Updated BoundedBuffer
    BoundedBuffer is now an array of chunks, issue is still occurring where
    the first ten seconds or so are being skipped. Debugging code shows that
    it is working as expected so the issue may be arising due to another
    class

--- StudentPlayerApplet.java
+++ StudentPlayerApplet.java
@@ -56,13 +56,14 @@ class Player extends Panel implements Runnable {
             line.open(format);
             line.start();
 
-            BoundedBuffer b = new BoundedBuffer(oneSecond);
+            BoundedBuffer b = new BoundedBuffer(10, oneSecond);
             
             Thread pthread = new Thread(new Producer(b, s, oneSecond));
-            pthread.start();
-
             Thread cthread = new Thread(new Consumer(b, line, oneSecond));
             cthread.start();
+            pthread.start();
+
+            
             
             pthread.join();
             cthread.join();
@@ -132,6 +133,7 @@ class Producer implements Runnable{ // read into buffer from AudioInputStream
     }
 }
 
+
 class Consumer implements Runnable{ //write to audio line
     public BoundedBuffer buffer;
     public SourceDataLine line;
@@ -148,11 +150,114 @@ class Consumer implements Runnable{ //write to audio line
         while(true){
             line.write(audioChunk, 0, oneSecond);
             audioChunk = buffer.removeChunk();
-            System.out.println(oneSecond);
         }
     }
 }
 
+class BoundedBuffer{
+    class Chunk{
+        byte[] arr;
+
+        Chunk(byte[] x){
+            arr = x;
+        }
+
+        byte[] getChunk(){
+            return arr;
+        }
+    }
+
+    Chunk[] arr;
+    int size;
+    int head;
+    int tail;
+    int length;
+    int bytes;
+    int guard;
+
+    BoundedBuffer(int l0, int b0){
+        length=l0; bytes=b0;
+        arr=new Chunk[l0];
+        head=0;
+        tail=0;
+        size=0;
+        guard=1;
+    }
+
+    public synchronized void insertChunk(byte[] x){
+        while(size==length||guard==0)try{wait();}catch(InterruptedException e){}
+        guard--;
+        arr[head] = new Chunk(x);
+        size++;
+        System.out.println("Queued at head position " + head + " size is " + size );
+        head=(head+1)%length;
+        guard++;
+        notifyAll();
+    }
+
+    public synchronized byte[] removeChunk(){
+        while(size==0||guard==0)try{wait();}catch(InterruptedException e){}
+        guard--;
+        byte[] x = arr[tail].getChunk();
+        size--;
+        System.out.println("Dequed at tail position " + tail + " size is " + size );
+        tail=(tail+1)%length;
+        guard++;
+        notifyAll();
+        System.out.println("Notified");
+        return x;
+    }
+}
+
+
+
+
+/*
+class BoundedBuffer{
+    byte[][] arr;
+    int size;
+    int head;
+    int tail;
+    int length;
+    int bytes;
+    int guard;
+
+    BoundedBuffer(int l0, int b0){
+        length=l0; bytes=b0;
+        arr=new byte[length][bytes];
+        head=0;
+        tail=0;
+        size=0;
+        guard=1;
+    }
+
+    public synchronized void insertChunk(byte[] x){
+        while(size==length||guard==0)try{wait();}catch(InterruptedException e){}
+        guard--;
+        arr[head] = x;
+        size++;
+        System.out.println("Queued at head position " + head + " size is " + size );
+        head++;
+        head=head%length;
+        guard++;
+        notifyAll();
+    }
+
+    public synchronized byte[] removeChunk(){
+        while(size==0||guard==0)try{wait();}catch(InterruptedException e){}
+        guard--;
+        byte[] x = arr[tail];
+        size--;
+        System.out.println("Dequed at tail position " + tail + " size is " + size );
+        tail++;
+        tail=tail%length;
+        guard++;
+        notifyAll();
+        return x;
+    }
+}
+*/
+/*
 class BoundedBuffer { // buffer could know about stream size
     private int nextIn, nextOut, size, occupied, ins, outs, sec;
     private boolean dataAvailable, roomAvailable;
@@ -160,7 +265,7 @@ class BoundedBuffer { // buffer could know about stream size
 
     // what role do ins/outs play?
     BoundedBuffer(int n) {// n should be one second
-        nextIn = 0; nextOut = 1; size = n; occupied = 0; ins = 0; outs = 0;
+        nextIn = 0; nextOut = 0; size = n; occupied = 0; ins = 0; outs = 0;
         dataAvailable = false;
         roomAvailable = true;
         sec=10;
@@ -169,41 +274,31 @@ class BoundedBuffer { // buffer could know about stream size
 
     public synchronized void insertChunk(byte[] audioChunk) {
         try {
-            while (!roomAvailable) {wait();}
-            roomAvailable=false;
+            while (occupied>=sec) {wait();}
             buffer[nextIn]=audioChunk;
             nextIn = (nextIn + 1) % sec;
-            occupied++; 
-            ins++; 
+            occupied++;
+            ins++;
             dataAvailable = true; 
             //roomAvailable = (occupied < sec-1) ? true : false;
-            if(occupied<sec)roomAvailable=true;
-            /*
-
-
-            so the problem seems to be that the way boundedbuffer is working right now, the buffer fills up to 10 seconds quickly, and then when the buffer is full it is writing over one extra spot. the consumer always ends up taking the most recently placed item instead of the oldest. Need to change something with the array placement to stop overwriting from occuring
-
-
-            next we may need to add a mixer so that volume can be changed instead of just playing from data line. this will allow pause and play.
-
-
-
-
-            */
+            if(ins-outs){
+                roomAvailable=false;
+            }
             notifyAll();
         } catch (InterruptedException e) { }
     }
 
     public synchronized byte[] removeChunk() {
         try {
-            while (!dataAvailable) {wait();}
-            dataAvailable=false;
+            while (occupied==0) {wait();}
             byte[] audioChunk = buffer[nextOut];
             nextOut = (nextOut + 1) % sec;
-            occupied--; 
-            outs++; 
+            occupied--;
+            outs++;
+            if(occupied==0){
+                dataAvailable=false;
+            }
             roomAvailable = true; 
-            if(occupied > 0) dataAvailable=true;
             notifyAll();
             return audioChunk;
         } catch (InterruptedException e) {
@@ -211,3 +306,5 @@ class BoundedBuffer { // buffer could know about stream size
             return null;}
     }
 }
+
+*/
